c657ea3977eff1e42ebbccd38055cc637dc92b3c
Perry Vorselaars <p.vorselaars@gmail.com>
2025-06-03 22:13:15 +0200

    Initial commit

---
+++ Simuload/Simulation.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+
+namespace Simuload;
+
+public record Result;
+public record Outcome;
+
+public class Simulation(Func<object, Task<Outcome>> scenario)
+{
+
+    public ILogger logger = LoggerFactory.Create(builder =>
+        {
+            builder.SetMinimumLevel(LogLevel.Information)
+                .AddOpenTelemetry(options =>
+                {
+                    options.SetResourceBuilder(ResourceBuilder.CreateDefault()
+                                .AddService("Simuload"))
+                                .AddConsoleExporter();
+                });
+
+        }).CreateLogger<Simulation>();
+
+    public async Task Run()
+    {
+
+        Stopwatch sw = new();
+
+        logger.LogTrace("Starting simulation");
+        sw.Start();
+        await scenario.Invoke(logger);
+        sw.Stop();
+        logger.LogTrace("Ending simulation");
+        logger.LogInformation($"Elapsed={sw.Elapsed}");
+    }
+
+};
---
+++ Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using Simuload;
+namespace Tests;
+
+[TestClass]
+public sealed class Tests
+{
+    [TestMethod]
+    public async Task CreateSimulation()
+    {
+        await new Simulation(async ctx =>
+        {
+            await Task.Delay(1000);
+            return new Outcome();
+        })
+        .Run();
+    }
+}
